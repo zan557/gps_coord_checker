eb7660c54af9bf3b4b68a94a600175075eda8daf
SuspiciousCrab <ziquan@myrlabs.com>
2023-07-28 15:01:26 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.9-slim
+
+WORKDIR /app
+
+COPY gpstoxy.py /app
+COPY requirements.txt /app
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["python", "gpstoxy.py"]
---
+++ gpstoxy.py
@@ -0,0 +1,52 @@
+
+from geographiclib.geodesic import Geodesic
+from typing import List
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+
+
+def is_valid_latitude(latitude: float) -> bool:
+    return -90.0 <= latitude <= 90.0
+
+def is_valid_longitude(longitude: float) -> bool:
+    return -180.0 <= longitude <= 180.0
+def distance_between_coords(coordA: List[float], coordB: List[float]) -> float:
+    if len(coordA) != 2 or len(coordB) != 2:
+        raise ValueError("Coordinates must have exactly two values (latitude and longitude)")
+
+    latitude_A, longitude_A = coordA
+    latitude_B, longitude_B = coordB
+
+    if not (is_valid_latitude(latitude_A) and is_valid_longitude(longitude_A)):
+        raise ValueError("Invalid latitude or longitude in coordA")
+
+    if not (is_valid_latitude(latitude_B) and is_valid_longitude(longitude_B)):
+        raise ValueError("Invalid latitude or longitude in coordB")
+
+    rslt=geod.Inverse(latitude_A,longitude_A,latitude_B,longitude_B)
+    distance= rslt["s12"]
+    return rslt
+
+legend={"lat1": "latitude of point 1","lon1":"longitude of point 1","lat2":"latitude of point 2","lon2":"longitude of point 2","a12":"spherical arc length from the first point to the second in degrees","s12":"the distance from the first point to the second in meters","azi1":"azimuth at the first point in degrees","azi2":"azimuth at the second point in degrees"}
+
+@app.route('/distance', methods=['POST'])
+def calc_dist():
+    try:
+        data = request.get_json()
+        coordA = data['coordA']
+        coordB = data['coordB']
+        rslt = distance_between_coords(coordA, coordB)
+        return jsonify({'distance': rslt})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route("/")
+def hello_world():
+    return "<p>Post JSON to /distance</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
