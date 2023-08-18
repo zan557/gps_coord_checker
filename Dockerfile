29329d58f55ee346571c4f84a3dbfb592e4ed667
ZQ <ziquan@myrlabs.com>
2023-08-18 19:41:24 +0800

    Messing with frontend

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /app
 
 COPY gpstoxy.py /app
 COPY requirements.txt /app
+COPY /templates /app/templates
+
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD ["python", "gpstoxy.py"]
--- gpstoxy.py
+++ gpstoxy.py
@@ -1,18 +1,20 @@
 
 from geographiclib.geodesic import Geodesic
 from typing import List
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
 geod = Geodesic.WGS84  # define the WGS84 ellipsoid
 
+mem_store = []
 
 def is_valid_latitude(latitude: float) -> bool:
     return -90.0 <= latitude <= 90.0
 
 def is_valid_longitude(longitude: float) -> bool:
     return -180.0 <= longitude <= 180.0
+
 def distance_between_coords(coordA: List[float], coordB: List[float]) -> float:
     if len(coordA) != 2 or len(coordB) != 2:
         raise ValueError("Coordinates must have exactly two values (latitude and longitude)")
@@ -28,6 +30,7 @@ def distance_between_coords(coordA: List[float], coordB: List[float]) -> float:
 
     rslt=geod.Inverse(latitude_A,longitude_A,latitude_B,longitude_B)
     distance= rslt["s12"]
+    mem_store.append({"coordA":coordA,"coordB":coordB,"distance":distance})
     return rslt
 
 legend={"lat1": "latitude of point 1","lon1":"longitude of point 1","lat2":"latitude of point 2","lon2":"longitude of point 2","a12":"spherical arc length from the first point to the second in degrees","s12":"the distance from the first point to the second in meters","azi1":"azimuth at the first point in degrees","azi2":"azimuth at the second point in degrees"}
@@ -46,7 +49,15 @@ def calc_dist():
 
 @app.route("/")
 def hello_world():
-    return "<p>Post JSON to /distance</p>"
+    return render_template('home.html')
+
+@app.route("/viewer")
+def display_map():
+    return jsonify(mem_store)
+
+@app.route("/coords", methods=["POST"])
+def store_coord():
+    return 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
